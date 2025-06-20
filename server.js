339115430f64c54603cb8031d520f44ac79c3360
kayyum99 <kmptl99@gmail.com>
2025-06-20 17:24:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const PORT = 8080;
 
 app.use(cors());
 
-const playlist = JSON.parse(fs.readFileSync("playlist.json", "utf-8"));
-
 app.get("/playlist", (req, res) => {
-  let output = "#EXTM3U\n";
-  playlist.forEach((channel) => {
-    output += `#EXTINF:-1 tvg-name="${channel.name}" group-title="IPTV",${channel.name}\n${channel.url}\n`;
-  });
-  res.type("text/plain").send(output);
+  try {
+    const data = fs.readFileSync("playlist.json", "utf-8");
+    const playlist = JSON.parse(data);
+    let output = "#EXTM3U\n";
+    playlist.forEach((channel) => {
+      output += `#EXTINF:-1 tvg-name="${channel.name}" group-title="IPTV",${channel.name}\n${channel.url}\n`;
+    });
+    res.type("text/plain").send(output);
+  } catch (err) {
+    console.error("Error loading playlist:", err.message);
+    res.status(500).send("Playlist load failed");
+  }
 });
 
 app.listen(PORT, () => {
